ac1ce201dd88b277c6133626a97719e95d44b501
Tonelyr <40433952+Tonelyr@users.noreply.github.com>
2020-08-16 14:14:57 -0400

    Update kid_writing.py

--- kid_writing.py
+++ kid_writing.py
@@ -1,17 +1,26 @@
-print("Welcome to the KiD wRiTiNg Generator !")
-print("11 characters max (for the moment)")
+print("Welcome to the KiD wRiTiNg Generator !") 
+
+userinput = input("Word to change: ")
+
+new_input = ""
+
+capital_letter = False
+
+for i in userinput:
+    if not capital_letter:
+        new_input += i.upper()
+        capital_letter = True
+    else:
+        new_input += i
+        capital_letter = False  
+print(new_input)
 
 
-useript = input("Word to change: ")
-str(useript)
-
-length = (len(useript))
-
-
+""" Update #1 (-1000 IQ)
 if length <= 1:
     print("There is nothing to transform")
 elif length == 2:
-    print(useript[:1] + useript[1].upper())
+    print(useript[:1].lower() + useript[1].upper())
 elif length == 3:
     print(useript[:1].lower() + useript[1].upper() + useript[2:].lower()) 
 elif length == 4:
@@ -31,4 +40,4 @@ elif length == 10:
 elif length == 11:
     print(useript[:1].lower() + useript[1].upper() + useript[2].lower() + useript[3].upper() + useript[4].lower() + useript[5].upper() + useript[6].lower() + useript[7].upper() + useript[8].lower() + useript[9].upper() + useript[10].lower())
 else:
-    print("Text too long ;-;")
+    print("Text too long ;-;") """
